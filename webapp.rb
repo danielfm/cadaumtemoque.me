a149089947000f3186538a040243a1da0906226a
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2012-01-31 22:25:04 -0200

    Escapa a string para evitar ataques.

--- webapp.rb
+++ webapp.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
 
+helpers do
+  include Rack::Utils
+
+  def oq
+    text = params[:oq] ? params[:oq] : "o"
+    escape_html text
+  end
+end
+
 get '/' do
   redirect '/rece', 302
 end
