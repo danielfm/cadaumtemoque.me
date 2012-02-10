335479f322c2a97dacb43526bcbc6d5f299b9da6
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2012-02-09 22:42:57 -0200

    Usando mongo para armazenar uma lista de frases pré-geradas para retornar aleatoriamente.

--- webapp.rb
+++ webapp.rb
@@ -1,13 +1,40 @@
+# -*- coding: utf-8 -*-
+
 require 'sinatra'
+require 'sinatra/mongo'
+
+# Environments
+
+configure :development do
+  set :mongo, 'mongodb://localhost:27017/cadaum'
+end
+
+configure :production do
+  set :mongo, ENV["MONGOLAB_URI"]
+end
+
+# Helpers
 
 helpers do
   include Rack::Utils
 
+  def random?
+    params[:oq] == "lol"
+  end
+
+  def random
+    return unless random?
+
+    oq = mongo["oq"]
+    oq.find_one({}, {skip: rand(oq.count)})["txt"]
+  end
+
   def oq
-    text = params[:oq] ? params[:oq] : "o"
-    escape_html text
+    escape_html random || (params[:oq] ? params[:oq] : "o")
   end
 end
+
+# Views
 
 get '/' do
   redirect '/rece', 302
